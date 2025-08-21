11a65b8cfef02f8b677ba4268326d75d7bab00a0
JakeOJeff <realjakeojeff@gmail.com>
2025-08-22 03:01:29 +0530

    Attempting to add a top circle

--- src/states/home.lua
+++ src/states/home.lua
@@ -8,15 +8,44 @@ function home:load()
     bottomnav:load(self)
 end
 
-
 function home:update(dt)
     bottomnav:update(dt)
 end
 
-
 function home:draw()
     love.graphics.setBackgroundColor(utils.hexToRgb(colors[4]))
 
+    love.gradient.draw(
+        function()
+            love.graphics.rectangle("fill", 0, 0, wW, wH)
+        end,
+        "linear",
+        wW / 2, wH / 2,          -- center x, y
+        wW, wH - bottomnav.height, -- width, height
+        utils.hexToRgb(colors[4]),
+        utils.hexToRgb(colors[5]),
+        0,   -- angle
+        1, 1 -- scale
+    )
+
+    local topCirc = {
+        x = 0,
+        y = 0,
+        w = wW,
+        h = wH/2
+    }
+    love.gradient.draw(
+        function ()
+            love.graphics.rectangle("fill", topCirc.x, topCirc.y, topCirc.w, topCirc.h, wW * scale, wW * scale)
+        end,
+        "linear",
+        topCirc.x/2, topCirc.y/2,  -- center x, y
+        topCirc.w, topCirc.h, -- width, height
+        utils.hexToRgb(colors[1]),
+        utils.hexToRgb(colors[3]),
+        0,   -- angle
+        1, 1 -- scale
+    )
     bottomnav:draw()
 end
 
