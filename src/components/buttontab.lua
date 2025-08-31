12b38cc986a3e87172e43315e8e47235b3112a1e
JakeOJeff <realjakeojeff@gmail.com>
2025-08-31 06:14:52 +0530

    Added basic drawing system for the buttons

--- src/components/buttontab.lua
+++ src/components/buttontab.lua
@@ -2,8 +2,14 @@ local buttontab = {}
 
 function buttontab:new()
     self.amounts = {
-        transaction_data
+        returnBalance()/3,
+        returnBalance()/2,
+        returnBalance()
     }
+    self.x = 10 * scale
+    self.y = 300 * scale
+    self.w = 50 * scale
+    self.h = 20 * scale
 end
 
 function buttontab:update(dt)
@@ -11,7 +17,10 @@ function buttontab:update(dt)
 end
 
 function buttontab:draw()
-    
+    love.graphics.setColor()
+    for i = 1, 3 do
+        love.graphics.rectangle("line", self.x + self.w * i, self.y, self.w, self.h, 10, 10)
+    end
 end
 
 return buttontab
